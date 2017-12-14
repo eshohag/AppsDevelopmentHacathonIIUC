65f4a60cb30f7b672707a654fca8331a89d91bf2
MD Shohag Mia <md-shohag-mia@outlook.com>
2017-12-14 16:27:02 +0600

    Initial Commit

---
+++ AppsDevelopmentHacathonIIUC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AppsDevelopmentHacathonIIUC.MvcApplication" Language="C#" %>
---
+++ AppsDevelopmentHacathonIIUC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AppsDevelopmentHacathonIIUC.Startup))]
+namespace AppsDevelopmentHacathonIIUC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
